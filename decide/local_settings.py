584f18ce0536d5bda4e890629aaa9e4bed867a67
Alex <awoo.public@gmail.com>
2019-08-30 12:43:35 +0200

    local_settings modified

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -32,6 +32,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'decide',
         'USER': 'decide',
+        'PASSWORD': 'decide',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
